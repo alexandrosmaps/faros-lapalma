a92a47b929dc7d3b9f8c914960563ade3fdfde3c
Àlex Cubells <47119384+alexandrosmaps@users.noreply.github.com>
2025-03-19 17:33:38 +0100

    Faros La Palma Plantilla

---
+++ config.js
@@ -0,0 +1,117 @@
+var config = {
+    style: 'mapbox://styles/alexandrosmaps/cm836stsm014401sdd0zo9cbb',
+    // leave commented to use Mapbox Standard Style
+    accessToken: 'pk.eyJ1IjoiYWxleGFuZHJvc21hcHMiLCJhIjoiY2xlNzJsZjVsMDBxNzNvcnozM2E4eGk5diJ9.Xld-N8aFRWlNsxMraKY1AQ',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    //projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: true,
+    insetOptions: {
+        markerColor: 'orange'
+    },
+    insetPosition: 'bottom-right',
+    theme: 'dark',
+    use3dTerrain: false, //set true for enabling 3D maps.
+    auto: false,
+    title: 'Faros La Palma',
+    subtitle: 'Storymaps de faros de la palma',
+    byline: 'Àlex Cubells',
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Faro de Punta Lava',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Faro_de_Punta_Lava_2014-11.JPG',
+            description: 'El faro de Punta Lava o faro de Punta del Moro es un faro situado en Tazacorte, al oeste de la isla de La Palma, en el archipiélago de las Islas Canarias, España. ',
+            location: {
+                center: [-17.925650701745003, 28.596670951137074],
+                zoom: 12.5,
+                pitch: 62.77,
+                bearing:70.28
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Faro de Fuencaliente',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/e/eb/At_La_Palma_2020_247.jpg',
+            description: 'El faro de Fuencaliente se encuentra en la punta sur de la isla de La Palma (Canarias, España), en el municipio de Fuencaliente.',
+            location: {
+                center: [-17.84307658092909, 28.455254782902472],
+                zoom: 12.5,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'third-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: 'Faro de Arenas Blancas',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/0/03/Faro_de_Arenas_Blancas_2014-11.JPG',
+            description: 'El faro de Arenas Blancas es un faro situado en la playa de Arenas Blancas, a 4 kilómetros al sureste de Lomo Oscuro, en la isla de La Palma, en el archipiélago de las Islas Canarias, España.',
+            location: {
+                center: [-17.76045751180869, 28.569917954227456],
+                zoom: 12.5,
+                pitch: 8.01,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fourth-chapter',
+            alignment: 'right',
+            hidden: false,
+            title: 'Faro de Punta Cumplida',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/e/e9/Faro_de_Punta_Cumplida.jpg',
+            description: 'El faro de Punta Cumplida es un faro situado en la localidad de Barlovento, en la isla de La Palma (provincia de Santa Cruz de Tenerife, Canarias, España).',
+            location: {
+                center: [-17.778057104461773, 28.83905657796037],
+                zoom: 12.5,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
